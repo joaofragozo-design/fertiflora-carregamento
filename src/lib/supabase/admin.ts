33624d21a1b3a20dfcfa1a1b9daf4d0f395e17b2
Joao <joao.fragozo@fertiflora.com>
2026-06-26 13:35:23 -0300

    Teste variáveis

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -1,3 +1,14 @@
-console.log("ADMIN CARREGOU");
+import { createClient } from "@supabase/supabase-js";
 
-export const supabaseAdmin = {} as any;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!url) {
+  throw new Error("NEXT_PUBLIC_SUPABASE_URL não encontrada");
+}
+
+if (!key) {
+  throw new Error("SUPABASE_SERVICE_ROLE_KEY não encontrada");
+}
+
+export const supabaseAdmin = createClient(url, key);
