6148af788e79e77e3de4590de1fc033c789528a0
Joao <joao.fragozo@fertiflora.com>
2026-07-13 11:41:34 -0300

    fix: unregister SW to eliminate interference with mobile auth

--- src/components/pwa/sw-register.tsx
+++ src/components/pwa/sw-register.tsx
@@ -4,8 +4,11 @@ import { useEffect } from 'react'
 
 export function SwRegister() {
   useEffect(() => {
+    // Desregistra qualquer SW anterior que possa estar interferindo
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').catch(() => {})
+      navigator.serviceWorker.getRegistrations().then((regs) => {
+        regs.forEach((r) => r.unregister())
+      })
     }
   }, [])
   return null
